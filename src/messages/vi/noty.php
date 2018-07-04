0bc2c6761d68fdfcfb5ed3a94cbf5199c3830443
Andrey <loveorigami@mail.ru>
2018-07-04 21:36:19 +0300

    Translation for Vietnamese

---
+++ src/messages/vi/noty.php
@@ -0,0 +1,10 @@
+return [
+    'Cancel' => 'Hủy',
+    'Confirmation Needed' => 'Yêu cầu xác nhận',
+    'Error' => 'Có lỗi',
+    'Info' => 'Thông tin',
+    'Ok' => 'Tốt',
+    'Question' => 'Câu hỏi',
+    'Success' => 'Thành công',
+    'Warning' => 'Cảnh báo',
+];
